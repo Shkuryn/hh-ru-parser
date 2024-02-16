7432c59f72a145458b8d3cbb83dead5f59365fcf
igor <shkuryn@gmail.com>
2024-02-17 00:56:31 +0200

    refactoring processor

--- lib/vacancy_processor.rb
+++ lib/vacancy_processor.rb
@@ -8,19 +8,7 @@ class VacancyProcessor
   end
 
   def call
-    title_elem = block.css('.serp-item__title').first
-    vacancy_title = title_elem ? title_elem.text.strip : 'No title found'
-
-    return nil unless vacancy_title.downcase.include?(title.downcase)
-
-    link_elem = block.css('.bloko-link').first
-    link = link_elem ? shorten_url(link_elem['href']) : 'No link found'
-
-    compensation_elem = block.css('.bloko-header-section-2').first
-    compensation = compensation_elem ? compensation_elem.text.strip.gsub(/\s+/, ' ') : '???'
-
-    employer_elem = block.css('.vacancy-serp-item__meta-info-company').first
-    employer_name = employer_elem ? employer_elem.css('a').first&.text&.strip : 'No employer found'
+    return nil unless relevant_vacancy?
 
     [vacancy_title, employer_name, compensation, link]
   end
@@ -28,6 +16,32 @@ class VacancyProcessor
   private
 
   attr_reader :block, :title
+
+  def relevant_vacancy?
+    vacancy_title.downcase.include?(title.downcase)
+  end
+
+  def vacancy_title
+    title_elem = block.css('.serp-item__title').first
+    title_elem ? title_elem.text.strip : 'No title found'
+  end
+
+
+  def link
+    link_elem = block.css('.bloko-link').first
+    link_elem ? shorten_url(link_elem['href']) : 'No link found'
+  end
+
+  def compensation
+    compensation_elem = block.css('.bloko-header-section-2').first
+    compensation_elem ? compensation_elem.text.strip.gsub(/\s+/, ' ') : '???'
+  end
+
+  def employer_name
+    employer_elem = block.css('.vacancy-serp-item__meta-info-company').first
+    employer_elem ? employer_elem.css('a').first&.text&.strip : 'No employer found'
+  end
+
 
   def shorten_url(original_url)
     uri = URI.parse("http://tinyurl.com/api-create.php?url=#{original_url}")

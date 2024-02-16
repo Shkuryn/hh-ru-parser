482de7d7b519397ca6d320e3fc49f8e00e6e4ef7
igor <shkuryn@gmail.com>
2024-02-16 12:05:03 +0200

    args join

--- index.rb
+++ index.rb
@@ -7,7 +7,7 @@ if __FILE__ == $PROGRAM_NAME
   title = if ARGV.count < 1
             'ruby'
           else
-            ARGV[0]
+            ARGV.join(' ')
           end
 
   vacancies = HeadHunterParser.new(title).parse_hh_vacancies
